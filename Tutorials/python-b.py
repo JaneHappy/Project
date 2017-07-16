113f1b7d2e5799df79adda2d0a37c9d0f5606a06
JaneHappy <1584360631@qq.com>
2017-07-16 23:40:29 +0800

    python chap 5 (not finished)

---
+++ Tutorials/python-b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+# chapter 5
+
+for letter in 'Python':		# Example 1
+	print 'Current letter: ', letter
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:		# e.g. 2
+	print 'Current fruit : ', fruit
+print "Good bye!"
+# eg 3
+for index in range(len(fruits)):
+	print 'Current fruit : ', fruits[index]
+print "Good bye!"
+
+
+
+
+# chapter 6
+
