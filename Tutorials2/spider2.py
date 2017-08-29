3b1944966c0670a52b54e6d322229865ef2103e4
JaneHappy <1584360631@qq.com>
2017-08-29 14:42:34 +0800

    spider.py

--- Tutorials2/spider2.py
+++ Tutorials2/spider2.py
@@ -1,31 +1,153 @@
 #!/usr/bin/env python
 #coding: utf-8
+from __future__ import print_function
+
+
+
+
+# PART I
+
+
+
+
+
+# PART 2
+
+
+
+
+# PART 3
+
+
+
+
+# PART 4
 '''
-网络爬虫（英语：web crawler），也叫网络蜘蛛（spider），是一种用来自动浏览万维网的网络机器人。其目的一般为编纂网络索引。
+from selenium import webdriver
+
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
 '''
 
-import os
+
+
+
+
+
+
+
+#----------------------------------
+
+
+
+#!/usr/bin/env python
+#coding: utf-8
+#from __future__ import print_function
+
+
+
+
+
+'''
+Class 2:
+
 # 导入 selenium
 from selenium import webdriver
 
-chromedriver = "C:\User\Lenovo\Downloads\chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
 # 打开谷歌浏览器，并访问百度
-browser = webdriver.Chrome(chromedriver)
+browser = webdriver.Chrome()
 browser.get('http://www.baidu.com/')
-browser.quit()  #driver
-#browser = webdriver.Chrome('http://www.baidu.com/')
+'''
 
 
 
 '''
-References:
+Class 2:
 
-2017.8.18 2:25
-http://selenium-python-zh.readthedocs.io/en/latest/installation.html
-https://stackoverflow.com/questions/8255929/running-selenium-webdriver-python-bindings-in-chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
+
+# 获取浏览器中 name 为 wd 的标签
+elem = driver.find_element_by_name("wd")
+# 搜索 cxy61
+elem.send_keys("cxy61")
+elem.send_keys(Keys.RETURN)
+# 打印页面
+print driver.page_source
+##driver.quit()
+'''
+
+
+
+# PART  III,  Class 3
+'''
+
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+
+dr = webdriver.Chrome()
+dr.get('https://www.qiushibaike.com/')
+
+# 获取 id 为 content-left 的元素
+main_content = dr.find_element_by_id('content-left')
+# 获取 class 为 content 的元素
+contents = main_content.find_elements_by_class_name('content')
+
+# for 循环
+i = 1
+for content in contents:
+	#print(content.text)
+	print(str(i)+'.' + content.text + '\n')
+	i += 1
+
+# 退出
+dr.quit()
 
 '''
+
+
+
+
+from selenium import webdriver
+
+class Qiubai:
+	def __init__(self):
+		self.dr = webdriver.Chrome()
+		self.dr.get('https://www.qiushibaike.com/')
+
+	def print_contenx(self):
+		main_content = self.dr.find_element_by_id('content-left')
+		contents = main_content.find_elements_by_class_name('content')
+
+		i = 1
+		for content in contents:
+			print(str(i)+"." + content.text + '\n')
+			i += 1
+
+		self.quit()
+
+	def quit(self):
+		self.dr.quit()
+
+
+Qiubai().print_contenx()
+
+
+
+
+
+
+
+
+
 
 
 
